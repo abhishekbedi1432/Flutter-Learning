7614890eb5a8eec636dbc6035a2818892dedf999
macintosh <macintosh@macintoshs-macbook-pro-2.local>
2019-01-06 01:50:55 +0530

    Added Interactivity to favourite button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,68 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
+class FavouriteWidget extends StatefulWidget {
+
+  @override
+  _FavouriteWidgetState createState() {
+    return _FavouriteWidgetState();
+  }
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavourite = true;
+  int _favouriteCount = 41;
+
+  void _toggleFavourite(){
+    setState(() {
+      if(_isFavourite) {
+        _isFavourite = false;
+        _favouriteCount -= 1;
+      }
+      else {
+        _isFavourite = true;
+        _favouriteCount += 1;
+
+      }
+    });
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+              icon: (_isFavourite ? Icon(Icons.star) : Icon(Icons.star_border)),
+              onPressed: _toggleFavourite
+          ),
+        ),
+        SizedBox(
+          width: 18.0,
+          child: Container(
+            child: Text('$_favouriteCount'),
+          ),
+        ),
+      ],
+
+    );
+
+  }
+
+
+}
+
+
+
+
+
+
+
 
 class MyApp extends StatelessWidget {
 
@@ -36,11 +98,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
+        FavouriteWidget(),
       ],
     ),
   );
